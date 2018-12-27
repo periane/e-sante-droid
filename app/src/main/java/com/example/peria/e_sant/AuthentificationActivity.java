fb405600f3ba129d7378673e48efb4fc16b12dd3
periane <perianetamo@gmail.com>
2018-12-27 13:56:11 +0100

    adding connection with activity

--- app/src/main/java/com/example/peria/e_sant/AuthentificationActivity.java
+++ app/src/main/java/com/example/peria/e_sant/AuthentificationActivity.java
@@ -1,13 +1,41 @@
 package com.example.peria.e_sant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AuthentificationActivity extends AppCompatActivity {
+public class AuthentificationActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn1, btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentification);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+        switch (view.getId()) {
+            case R.id.btn1:
+                i = new Intent(this, LoginActivity.class);
+                startActivity(i)
+                ;
+                break;
+            case R.id.btn2:
+                i = new Intent(this, LoginActivity.class);
+                startActivity(i);
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/peria/e_sant/MainActivity.java
+++ app/src/main/java/com/example/peria/e_sant/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.peria.e_sant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -10,34 +13,42 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView image1, image2, image3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        image1 = (ImageView) findViewById(R.id.image1);
+        image2 = (ImageView) findViewById(R.id.image2);
+        image3 = (ImageView) findViewById(R.id.image3);
 
-        /*myRef.setValue("Hello, World!");*/
+        image1.setOnClickListener(this);
+        image2.setOnClickListener(this);
+        image3.setOnClickListener(this);
+    }
 
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d("test", "Value is: " + value);
-            }
+    @Override
+    public void onClick(View view) {
+        Intent i;
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("test", "Failed to read value.", error.toException());
-            }
-        });
+        switch (view.getId()) {
+            case R.id.image1:
+                i = new Intent(this, AuthentificationActivity.class);
+                startActivity(i);
+                break;
+            case R.id.image2:
+                i = new Intent(this, AuthentificationActivity.class);
+                startActivity(i);
+                break;
+            case R.id.image3:
+                i = new Intent(this, AuthentificationActivity.class);
+                startActivity(i);
+                break;
+                default:break;
+        }
     }
 }
